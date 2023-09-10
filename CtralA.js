88259290b093499e9e985cb0bf8f9948c30d7b78
Haku <89026040+sakanaJA@users.noreply.github.com>
2023-09-10 18:15:05 +0900

    CtrlA

--- CtralA.js
+++ CtralA.js
@@ -1,8 +1,10 @@
 document.getElementById('inputField').addEventListener('keydown', function(event) {
     // Ctrl + A を検出
     if (event.ctrlKey && event.key === 'a') {
-        document.getElementById('result').innerText = '成功! Ctrl + A が押されました！\n'
-        +'※補足：テキストボックス内をダブルクリックするとその文字と同じ種類の文字が連続する範囲が選択されるぞ！';
+        alert('成功！\n'
+        +'※補足：テキストボックス内が全選択されました。');
+        document.getElementById('result').innerText = 'テキストボックス内を全選択に成功！\n'
+        +'※Ctrl+Cと組み合わせるとテキストボックス内を全コピーすることができるよ！';
         //event.preventDefault();  // テキスト全選択のデフォルト動作を阻止
     }
 });
